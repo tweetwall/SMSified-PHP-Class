64ec91e4a84daec62807fe0b8d4f55263b1088f1
Joel Strellner <joel@socialping.com>
2011-05-20 08:34:37 -0700

    Fixed typos; Added clarifications

--- example_send.php
+++ example_send.php
@@ -7,14 +7,14 @@ require('class.smsified.php');
 
 $SMSified = new SMSified('YOUR_USERNAME', 'YOUR_PASSWORD', 'YOUR_ASSIGNED_PHONE_NUMBER');
 
-$SMSififed_send = $SMSified->SendSMS('11_DIGIT_PHONE_NUMBER_TO','This is where you put your test message.');
-if ($SMSififed_send === false) {
+$SMSified_send = $SMSified->SendSMS('11_DIGIT_PHONE_NUMBER_TO','This is where you put your test message.');
+if ($SMSified_send === false) {
 	echo 'SMS Sending Failed!';
 }
 else {
 	echo 'SMS Sent!';
 	
-	// since $SMSififed_send is not false, it should contain the $requestId assigned by SMSified. Store if needed.
-	echo $SMSififed_send;
+	// since $SMSified_send is not false, it should contain the $requestId assigned by SMSified. Store if needed.
+	echo $SMSified_send;
 }
 ?>
